784094716d5dda62ad63e4ec8bc8ea9ac2fded83
Piotr Grochowski <piogrek@gmail.com>
2015-04-07 12:19:12 +0100

    adding style overrides (#1545)

--- functions.php
+++ functions.php
@@ -28,7 +28,9 @@
     function devdmbootstrap3_theme_stylesheets()
     {
         wp_register_style('bootstrap.css', get_template_directory_uri() . '/css/bootstrap.css', array(), '1', 'all' );
+        wp_register_style('theme-main', get_template_directory_uri() . '/css/main.css', array(), '1', 'all' );
         wp_enqueue_style( 'bootstrap.css');
+        wp_enqueue_style( 'theme-main');
         wp_enqueue_style( 'stylesheet', get_stylesheet_uri(), array(), '1', 'all' );
     }
     add_action('wp_enqueue_scripts', 'devdmbootstrap3_theme_stylesheets');
