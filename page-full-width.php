91dd7493d5a79cb86533fd0fd0e2dd19848589e3
Piotr Grochowski <piogrek@gmail.com>
2015-04-07 12:51:25 +0100

    full width template fix

--- page-full-width.php
+++ page-full-width.php
@@ -13,7 +13,7 @@
 get_header();
 ?>
 
-<?php get_template_part('template-part', 'head'); ?>
+<?php get_template_part('template-part', 'head-fullwidth'); ?>
 
 <?php get_template_part('template-part', 'topnav'); ?>
 
